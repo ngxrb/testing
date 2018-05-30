6ad02c76258f118c8859416902643b9787980959
Ricardo Javier Barrios Díaz <ricardojbarrios@gmail.com>
2018-05-30 12:08:43 +0100

    docs(IsEmittedArgs): added api docs

--- src/lib/models/is-emitted-args.model.ts
+++ src/lib/models/is-emitted-args.model.ts
@@ -1,9 +1,26 @@
 export class IsEmittedArgs {
+  /**
+   * The EventEmitter property.
+   */
   public output: any;
+  /**
+   * The emitted value.
+   */
   public value: any;
+  /**
+   * (Optional) The method that fires the event to test.
+   */
   public onMethod?: any;
+  /**
+   * (Optional) The args needed to invoke the method that fires the event to test.
+   */
   public onMethodArgs?: any[];
 
+  /**
+   * Creates an instance of IsEmittedArgs.
+   * @param output The EventEmitter property.
+   * @param value The emitted value.
+   */
   constructor(output: any, value: any) {
     this.output = output;
     this.value = value;
